9646f12761003b7ce76a364d2b98775841611b3f
Dzianis .L <leadzian213@gmail.com>
2023-03-21 16:55:51 +0300

    task 5 (#4)
    task 5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://mil2oe0tfc.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://xun888d5wf.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
